5a579aaa2a45185fa2c84b195d7056579b61d621
Jérémy Bojko <jeremy.bojko@prontonmail.com>
2021-02-19 16:26:20 +0100

    Add delete feature

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ const buttonAdd = document.getElementById('buttonAddElement');
 let todoList = document.getElementById('todoListElement');
 
 buttonAdd.addEventListener('click', function() {
-  let contentValue = inputText.value; 
+  let contentValue = inputText.value;
+  inputText.value = "";
   console.log(contentValue);
-  todoList.innerHTML += '<li>'+contentValue+'</li>';
+  todoList.innerHTML += '<li>'+contentValue+'<button class="btnSuppr">Supprimer</button></li>';
+  AddEventSuppr();
 });
 
+function AddEventSuppr() {
+  let listBtn = document.getElementsByClassName('btnSuppr');
+  
+  for (let index = 0; index < listBtn.length; index++) {
+    listBtn[index].addEventListener('click', function(event) {
+      event.target.parentElement.remove();
+    })   
+  }
+}
