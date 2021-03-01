57222bf0d3362d468c1cdf3a2566af1553241942
Jérémy Bojko <jeremy.bojko@prontonmail.com>
2021-03-01 15:17:38 +0100

    Edit element

--- js/main.js
+++ js/main.js
@@ -7,16 +7,36 @@ buttonAdd.addEventListener('click', function() {
   let contentValue = inputText.value;
   inputText.value = "";
   console.log(contentValue);
-  todoList.innerHTML += '<li>'+contentValue+'<button class="btnSuppr">Supprimer</button></li>';
-  AddEventSuppr();
+  todoList.innerHTML += '<li>'+contentValue+'<button class="btnEdit">Modification</button><button class="btnSuppr">Supprimer</button></li>';
+  AddEventBtn();
 });
 
-function AddEventSuppr() {
-  let listBtn = document.getElementsByClassName('btnSuppr');
+function AddEventBtn() {
+  let listBtnSuppr = document.getElementsByClassName('btnSuppr');
+  let listBtnEdit = document.getElementsByClassName('btnEdit');
   
-  for (let index = 0; index < listBtn.length; index++) {
-    listBtn[index].addEventListener('click', function(event) {
+  for (let index = 0; index < listBtnSuppr.length; index++) {
+    listBtnSuppr[index].removeEventListener('click');
+    listBtnEdit[index].removeEventListener('click');
+
+    listBtnSuppr[index].addEventListener('click', function(event) {
       event.target.parentElement.remove();
-    })   
+    }); 
+
+    listBtnEdit[index].addEventListener('click', function(event) {
+      EditEventBtn(event);
+    }); 
   }
 }
+
+function EditEventBtn(event) {
+  let temp = event.target.parentElement;
+  console.log(temp);
+  event.target.parentElement.innerHTML = '<input type="text" id="newValue"><button id="btnSave">Sauvegarder</button>'
+
+  btnSave.addEventListener('click', function(){
+    let newValueItem = document.getElementById('newValue').value;
+    temp.innerHTML =  newValueItem+'<button class="btnEdit">Modification</button><button class="btnSuppr">Supprimer</button>';
+    AddEventBtn();
+  })
+} 
